6a81c678c067500e75f422b4577cc0024f74b211
Casey Bowman <caseybowman@bpccm.com>
2015-11-22 04:05:42 -0800

    Updated main to have voice recognition.

--- app/src/main/java/com/psu/bowmanagrawal/speakyourownadventure/MainActivity.java
+++ app/src/main/java/com/psu/bowmanagrawal/speakyourownadventure/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.media.Image;
 import android.os.Bundle;
 import android.content.Intent;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,19 +14,96 @@ import android.view.View.OnTouchListener;
 import android.speech.SpeechRecognizer;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import java.util.ArrayList;
+import android.util.Log;
 
 
 public class MainActivity extends Activity implements OnTouchListener {
 
     private SpeechRecognizer speechRecognizer;
+    private String outputStr = "";
+    static final String TAG = "error_logger";
+    ImageButton speechButton; //= (ImageButton) findViewById(R.id.speech_button);
+    TextView textOutput; //= (TextView) findViewById(R.id.text_output);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        ImageButton speechButton = (ImageButton) findViewById(R.id.speech_button);
+
+        speechButton = (ImageButton) findViewById(R.id.speech_button);
+        textOutput = (TextView) findViewById(R.id.text_output);
+
         final Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        RecognitionListener recognitionListener = new RecognitionListener() {
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> voiceResults = results
+                        .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (voiceResults == null) {
+                    Log.e(TAG, "No voice results");
+                    textOutput.setText("No voice results");
+                } else {
+                    Log.d(TAG, "Printing matches: ");
+                    for (String match : voiceResults) {
+                        Log.d(TAG, match);
+                    }
+                    outputStr = voiceResults.get(0);
+                    textOutput.setText(outputStr);
+                }
+            }
+
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+                Log.d(TAG, "Ready for speech");
+            }
+
+            @Override
+            public void onError(int error) {
+                Log.d(TAG,
+                        "Error listening for speech: " + error);
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                Log.d(TAG, "Speech starting");
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+                // TODO Auto-generated method stub
+
+            }
+        };
+
+        speechRecognizer.setRecognitionListener(recognitionListener);
+
         OnTouchListener touchListener = new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent event) {
@@ -37,11 +115,14 @@ public class MainActivity extends Activity implements OnTouchListener {
                     case MotionEvent.ACTION_UP:
                         // End
                         speechRecognizer.stopListening();
+                        //outputStr = speechRecognizer.RESULTS_RECOGNITION;
+                        //textOutput.setText(outputStr);
                         break;
                 }
                 return false;
             }
         };
+
 
         speechButton.setOnTouchListener(touchListener);
     }
@@ -58,9 +139,11 @@ public class MainActivity extends Activity implements OnTouchListener {
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 // Start
+                textOutput.setText("Listening...");
                 break;
             case MotionEvent.ACTION_UP:
                 // End
+                textOutput.setText("Listened");
                 break;
         }
         return false;
