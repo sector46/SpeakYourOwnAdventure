158ff01323e17591eeace2729212cc966bc99d1f
Casey Bowman <caseybowman@bpccm.com>
2015-12-01 00:28:35 -0800

    Reorganized the layouts and connected the start page buttons to activites

---
+++ app/src/main/java/com/psu/bowmanagrawal/speakyourownadventure/Credits.java
@@ -0,0 +1,16 @@
+package com.psu.bowmanagrawal.speakyourownadventure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by caseybowman on 12/1/15.
+ */
+public class Credits extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credits_page);
+    }
+}
